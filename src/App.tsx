e8daf37dae0b2bf805e1e6106735d69877ec8a9d
Pranav Shamkuwar <pranavshamkuwar92@gmail.com>
2026-09-12 00:29:56 +0530

    Created routes in app.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import { Routes, Route }from "react-router-dom"
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Projects } from "./pages/Projects";
+import { Contact } from "./pages/Contact";
+
 function App() {
   return (
-    <div>
-      <h1>
-        My Portfolio
-      </h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="about" element={<About />}/>
+      <Route path="projects" element={<Projects />}/>
+      <Route path="contact/" element={<Contact />}/>
+    </Routes>
   )
 }
 
