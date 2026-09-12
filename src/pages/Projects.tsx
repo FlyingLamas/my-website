1ebca12b15d06211330eda536a7d4bc3f1592438
Pranav Shamkuwar <pranavshamkuwar92@gmail.com>
2026-09-12 10:14:18 +0530

    Updated projects.tsx file

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,5 +1,13 @@
+import ExpenseTracker from "../assets/images/expense-tracker.png"
+import "./projects.css"
+
 export function Projects() {
     return (
-        <h1>Projects</h1>
+        <div>
+            <h1>Projects</h1>
+            <div className="project-image-container">
+                <img className="project-image" src={ExpenseTracker} alt="Expense Tracker Project" />
+            </div>
+        </div>
     );
 }
