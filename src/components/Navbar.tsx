3cf0e8fc6861111c7d9cc465fb3bec2bfbbae0a5
Pranav Shamkuwar <pranavshamkuwar92@gmail.com>
2026-09-12 20:27:29 +0530

    Added css styling to projects and navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,9 @@
 import { NavLink } from "react-router-dom";
+import "./Navbar.css"
 
 export function Navbar() {
     return (
-        <div>
+        <div className="navbar">
             <NavLink to="/">Home</NavLink>
             <NavLink to="/about">About</NavLink>
             <NavLink to="/projects">Projects</NavLink>
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -7,6 +7,10 @@ export function Projects() {
             <h1>Projects</h1>
             <div className="project-image-container">
                 <img className="project-image" src={ExpenseTracker} alt="Expense Tracker Project" />
+                <div className="project-name">
+                    <h3>Expense Tracker</h3>
+                    <p>A personal finance application built to monitor monthly budgets and track expenses.</p>
+                </div>
             </div>
         </div>
     );
